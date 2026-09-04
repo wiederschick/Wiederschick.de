554bb23d11d2926cc813b34b709a904cdb177087
wiederschick <andi-neubauer@gmx.de>
2026-09-04 11:23:59 +0200

    Refactor menu button and add analytics script
    Refactor menu button event listener and add Cloudflare Web Analytics script.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-const b=document.querySelector('.menu-btn'),n=document.querySelector('.navlinks');if(b&&n)b.addEventListener('click',()=>n.classList.toggle('open'));
+const b = document.querySelector('.menu-btn');
+const n = document.querySelector('.navlinks');
+
+if (b && n) {
+  b.addEventListener('click', () => n.classList.toggle('open'));
+}
+
+/* Cloudflare Web Analytics */
+if (!document.querySelector('script[src*="static.cloudflareinsights.com/beacon.min.js"]')) {
+  const cf = document.createElement('script');
+  cf.type = 'module';
+  cf.src = 'https://static.cloudflareinsights.com/beacon.min.js';
+  cf.setAttribute(
+    'data-cf-beacon',
+    '{"token":"51c1c61ded684e939312e111d2624723"}'
+  );
+  document.body.appendChild(cf);
+}
